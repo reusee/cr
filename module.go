08d2762b201d10a12de56c3cb472e38229eb8e8e
reus <reusee@gmail.com>
2021-12-14 12:30:10 +0800

    check command line argument for base dir

--- module.go
+++ module.go
@@ -8,9 +8,16 @@ import (
 type ModuleDir string
 
 func (_ Global) ModuleDir() ModuleDir {
-	// find go.mod
-	dir, err := filepath.Abs(".")
+	var dir string
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	}
+	if dir == "" {
+		dir = "."
+	}
+	dir, err := filepath.Abs(dir)
 	ce(err)
+	// find go.mod
 	for {
 		_, err := os.Stat(filepath.Join(dir, "go.mod"))
 		if err == nil {
